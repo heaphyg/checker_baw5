b506be616b8a26315fa8fc7d6d65c54434738768
Apprentice <apprentice@dbc-chicago-004.local>
2014-04-24 18:08:48 -0500

    Added valitations and fixed some associations.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
-	belongs_to :winner, classname: 'User', foreign_key: 'winner_id'
-	belongs_to :loser, classname: 'User', foreign_key: 'loser_id'
+	belongs_to :winner, class_name: 'User', foreign_key: 'winner_id'
+	belongs_to :loser, class_name: 'User', foreign_key: 'loser_id'
 	has_many :usergames
 	has_many :users, through: :usergames
 	has_many :moves
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 	has_secure_password
 	has_many :usergames
 	has_many :games, through: :usergames
