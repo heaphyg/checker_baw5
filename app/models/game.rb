284c334f1db8263ac8a91bfcb8517570e6b9c701
Apprentice <apprentice@dbc-chicago-004.local>
2014-04-24 18:22:22 -0500

    Added method to check if destination is empty.
    Added method to check if the destination is a valid distance from the starting position.
    For a regular move the distance should only be one.
    For a jump move the distance should only be two.

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,28 @@ class Game < ActiveRecord::Base
 	has_many :usergames
 	has_many :users, through: :usergames
 	has_many :moves
+
+  def empty_space?(end_loc)
+    Board[end_loc[0]][end_loc[1]] == ''
+  end
+
+  def valid_move?(start_loc, end_loc)
+    move_dist = calc_move_distance(start_loc, end_loc)
+    curr_row = start_loc[0]
+    curr_col = start_loc[1]
+    dest_row = end_loc[0]
+    dest_col = end_loc[1]
+
+    if piece_color == "black" # black should be on top
+      player_direction = -move_dist
+    else
+      player_direction = move_dist
+    end
+
+    if dest_row == (curr_row - player_direction) && dest_col == (curr_col + move_dist || curr_col - move_dist)
+      return true
+    else
+      return false
+    end
+  end
 end
