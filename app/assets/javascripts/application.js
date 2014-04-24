3bb59edde6a52ebd3da95d3bcf411349cde20af8
Apprentice <apprentice@dbc-chicago-007.local>
2014-04-24 18:01:47 -0500

    Returning the board with one piece, returning array of selected_position, and new_position

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,61 @@
 //= require websocket_rails/main
 //= require_tree .
 
+click_holder = []
+
+Game = {
+  init: [],
+}
+
+function Player (name) {
+  this.name = name
+};
+
+
+function Game () {
+  this.click_holder = []
+};
+
+
+
+$(document).ready(function(){
+  // $('.board').hide();
+
+
+$('form').submit(function(event){
+  event.preventDefault();
+  var p1name = $('input[name="player1"]').val();
+  var p2name = $('input[name="player2"]').val();
+
+  console.log(p1name);
+  console.log(p2name);
+
+
+  //show board and display pieces
+  $('.board').show();
+  $('#00').html("<div class='piece black'></div>");
+
+  $('.game-menu').hide()
+  alert('Player 1 make your move');
+
+});
+
+
+
+//passing data back to game logic controller
+  $('.board-cell').click(function(){
+      var selected_position = $(this).attr('id');
+      if (click_holder.length < 2 ) {
+        click_holder.push(selected_position)
+        console.log(click_holder)
+      }
+
+
+      // $.post("/game/position", data, function(response){
+          //what to do with the response
+
+      // });
+  });
+
+
+});
