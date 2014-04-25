be4d5996e8e7616cd5b187dd783c344aca8c7718
Apprentice <apprentice@dbc-chicago-007.local>
2014-04-25 11:27:13 -0500

    loaded board, moved piece, still fine-tuning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,52 +26,55 @@ function Player(name) {
     this.name = name
 };
 
-
 function Game() {
     this.click_holder = []
 };
 
-
-
 $(document).ready(function() {
-    $('.board').hide();
-
+    // $('.board').hide();
 
     $('.game-menu').submit(function(event){
       event.preventDefault();
       var p1name = $('input[name="player1"]').val();
       var p2name = $('input[name="player2"]').val();
 
-
         console.log(p1name);
         console.log(p2name);
 
-
         //show board and display pieces
         $('.board').show();
-        $('#00').html("<div class='piece black'></div>");
-
+        // $('#00').html("<div class='piece black'></div>");
         $('.game-menu').hide()
         alert('Player 1 make your move');
 
     });
-
-
 
     //passing data back to game logic controller -
     $('.board-cell').click(function() {
         var selected_position = $(this).attr('id');
         if (click_holder.length < 2) {
             click_holder.push(selected_position)
+
+            $("#"+click_holder[0]).html("");
+            $("#" + click_holder[1]).html("<div class='piece black'></div>");
             console.log(click_holder)
         }
 
-
         // $.post("/game/position", data, function(response){
         //what to do with the response
-
         // });
-    });
 
+        var response = true;
+
+        if (response) {
+            click_holder = [];
+            alert('Awesome Move! Next player\'s turn');
+        } else {
+            alert('Illegal Move! Try Again!');
+            $("#" + click_holder[0]).html("<div class='piece black'></div>");
+            click_holder = [];
+        }
+
+    });
 
 });
