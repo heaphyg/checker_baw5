f9536a16395795914401b2e1906cfee95bca7567
Apprentice <apprentice@dbc-chicago-004.local>
2014-04-25 20:04:12 -0500

    move_distance_calculation tests pass - on to valid move test

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,4 +11,41 @@ class Game < ActiveRecord::Base
     Board.where(coord: end_loc).first.is_occupied
   end
 
+  def self.move_distance_calculation(start_loc, end_loc)
+    start_loc = start_loc.split('').map(&:to_i)
+    end_loc = end_loc.split('').map(&:to_i)
+    curr_row = start_loc[0]
+    curr_col = start_loc[1]
+    dest_row = end_loc[0]
+    dest_col = end_loc[1]
+
+    if (curr_row - dest_row) == (curr_col - dest_col)
+      move_dist = curr_row - dest_row
+    else
+      move_dist = 2
+    end
+    return move_dist
+  end
+
+    def self.valid_move?(start_loc, end_loc)
+      move_dist = move_distance_calculation(start_loc, end_loc)
+      curr_row = start_loc[0]
+      curr_col = start_loc[1]
+      dest_row = end_loc[0]
+      dest_col = end_loc[1]
+
+      if piece_color == "black" # black should be on top
+        player_direction = -move_dist
+      else
+        player_direction = move_dist
+      end
+
+      if dest_row == (curr_row - player_direction) && dest_col == (curr_col + move_dist || curr_col - move_dist)
+        return true
+      else
+        return false
+    end
+  end
+
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -25,11 +25,43 @@ describe Game do
   context ".empty_space?" do
     Board.create(coord:'31', is_occupied: false, game_id: 1)
     Board.create(coord:'00', is_occupied: true, game_id: 1)
-    it "returns true if the destination space is unoccupied" do
+
+    it "method returns false if the destination space is unoccupied" do
       expect(Game.empty_space?('31')).to eq false
     end
-    it "returns false if the destination space is occupied" do
+    it "method returns true if the destination space is occupied" do
       expect(Game.empty_space?('00')).to eq true
+    end
+
+    context ".move_distance_calculation" do
+      it "calulates the distance between start_loc and end_loc for a contiguous destination" do
+        expect(Game.move_distance_calculation('51', '40')).to eq 1
+      end
+
+      it "calculates the distance between start_loc and end_loc for a jump" do
+        expect(Game.move_distance_calculation('51', '33')).to eq 2
+      end
+    end
+
+    context ".valid_move?" do
+      it "method returns true if the move is valid (non jump move)" do
+        expect(Game.valid_move?('51', '41')).to eq true
+      end
+
+      # it "method returns true if the move is valid (jump move)" do
+      #   expect(Game.valid_move?('51', '33')).to eq true
+      # end
+
+      # it "method returns false if the move is invalid" do
+      #   expect(Game.valid_move?('51', '43')).to eq false
+      # end
     end
   end
  end
+
+
+
+
+
+
+
