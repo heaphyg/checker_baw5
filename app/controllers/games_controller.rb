68b217088e70f4780c81b476a0a95e9b8656f8cd
Greg Heaphy <heaphyg@gmail.com>
2014-04-26 14:49:59 -0500

    updated the controller with with new board and piece tables. we aslo return true and false as strings to the view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,31 +1,37 @@
 class GamesController < ApplicationController
-  def conduct_game
+  def make_move
     game_id = session[:game_id]
-    start_loc = params[:start_loc].split('').map(&:to_i)
-    end_loc = params[:end_loc].split('').map(&:to_i)
-    piece_id = Move.where(position: start_loc).first.id
-    new_loc = end_loc*'' # turns array of two numbers into string
-    midpoint = find_square_between_origin_and_destination(start_loc, end_loc)
-    opponent_piece_id = Move.where(position: midpoint).first.id
+    start_loc = params[:start_loc]
+    end_loc = params[:end_loc]
+    unique_piece_id = param[:unique_piece_id]
+    midpoint = Game.find_square_between_origin_and_destination(start_loc, end_loc)
 
-    if empty_space?(end_loc) && valid_move?(start_loc, end_loc)
-      move_distance = move_distance_calculation(start_loc, end_loc)
+    if Game.empty_space?(end_loc) && Game.valid_move?(start_loc, end_loc)
+      move_distance = Game.move_distance_calculation(start_loc, end_loc)
+
       if move_distance == 1
-          Move.create(piece_id: piece_id,  game_id: game_id, position: new_loc)
+          Board.where(coord: start_loc, game_id: game_id).first.is_occupied = false
+          Board.where(coord: end_loc, game_id: game_id).first.is_occupied = true
+          Piece.where(unique_piece_id: unique_piece_id).first.location = end_loc
+
+          return 'true'
       elsif move_distance == 2
-        if opponent_in_jump_midpoint?(start_loc, end_loc)
-          Move.create(piece_id: piece_id, game_id: game_id, position: new_loc)
-          Move.create(piece_id: opponent_piece_id, game_id: game_id, position: '')
-          if game_over?(opponent_piece_id, game_id)
-            # End game route
-          else
-            # Continue game route
-          end
+        if Game.opponent_in_jump_midpoint?(start_loc, end_loc)
+          Board.where(coord: start_loc, game_id: game_id).first.is_occupied = false
+          Board.where(coord: end_loc, game_id: game_id).first.is_occupied = true
+          Piece.where(unique_piece_id: unique_piece_id).first.location = end_loc
+
+          Board.where(coord: midpoint, game_id: game_id).first.is_occupied = false
+          Piece.where(position: midpoint).first.location = ''
+
+          return 'true'
         else
-          @error = "Invalid move!"
+          # @error = "Invalid move!"
+          return 'false'
         end
       else
-        @error = "Invalid move!"
+        # @error = "Invalid move!"
+        return 'false'
       end
     end
   end
