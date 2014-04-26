0caf16d2367186cb9cf1f0e78eb77ec5c89dd77d
Javad Afshar <mjafshar@gmail.com>
2014-04-26 13:37:49 -0500

    Got the .opponent_in_jump_midpoint? method tests and method to work.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe Game do
-  # let(:game) { Game.new }
-
-  # context "Game object instantiation" do  # we don't have an initialize method
-  #   it "creates a GameController object" do
-  #     expect(game).to be_an_instance_of Game
-  #   end
-  # end
   it { should have_db_column(:winner_id).of_type(:integer) }
   it { should have_db_column(:loser_id).of_type(:integer) }
 
@@ -149,23 +142,74 @@ describe Game do
   end
 
   context ".opponent_in_jump_midpoint?" do
-    Board.create(coord: '31', is_occupied: false, game_id: 1)
-    Board.create(coord: '33', is_occupied: true, game_id: 1)
-    Board.create(coord: '35', is_occupied: true, game_id: 1)
-    Board.create(coord: '46', is_occupied: true, game_id: 1)
-
+    Board.create(coord:'00', is_occupied: true, game_id: 1)
+    Board.create(coord:'02', is_occupied: true, game_id: 1)
+    Board.create(coord:'04', is_occupied: true, game_id: 1)
+    Board.create(coord:'06', is_occupied: true, game_id: 1)
+    Board.create(coord:'11', is_occupied: true, game_id: 1)
+    Board.create(coord:'13', is_occupied: true, game_id: 1)
+    Board.create(coord:'15', is_occupied: true, game_id: 1)
+    Board.create(coord:'17', is_occupied: true, game_id: 1)
+    Board.create(coord:'20', is_occupied: true, game_id: 1)
+    Board.create(coord:'22', is_occupied: true, game_id: 1)
+    Board.create(coord:'24', is_occupied: true, game_id: 1)
+    Board.create(coord:'26', is_occupied: true, game_id: 1)
+    Board.create(coord:'30', is_occupied: false, game_id: 1)
+    Board.create(coord:'31', is_occupied: false, game_id: 1)
+    Board.create(coord:'32', is_occupied: false, game_id: 1)
+    Board.create(coord:'33', is_occupied: false, game_id: 1)
+    Board.create(coord:'34', is_occupied: false, game_id: 1)
+    Board.create(coord:'35', is_occupied: false, game_id: 1)
+    Board.create(coord:'36', is_occupied: false, game_id: 1)
+    Board.create(coord:'37', is_occupied: false, game_id: 1)
+    Board.create(coord:'40', is_occupied: false, game_id: 1)
+    Board.create(coord:'41', is_occupied: false, game_id: 1)
+    Board.create(coord:'42', is_occupied: false, game_id: 1)
+    Board.create(coord:'43', is_occupied: false, game_id: 1)
+    Board.create(coord:'44', is_occupied: false, game_id: 1)
+    Board.create(coord:'45', is_occupied: false, game_id: 1)
+    Board.create(coord:'46', is_occupied: false, game_id: 1)
+    Board.create(coord:'47', is_occupied: false, game_id: 1)
+    Board.create(coord:'51', is_occupied: true, game_id: 1)
+    Board.create(coord:'53', is_occupied: true, game_id: 1)
+    Board.create(coord:'55', is_occupied: true, game_id: 1)
+    Board.create(coord:'57', is_occupied: true, game_id: 1)
+    Board.create(coord:'60', is_occupied: true, game_id: 1)
+    Board.create(coord:'62', is_occupied: true, game_id: 1)
+    Board.create(coord:'64', is_occupied: true, game_id: 1)
+    Board.create(coord:'66', is_occupied: true, game_id: 1)
+    Board.create(coord:'71', is_occupied: true, game_id: 1)
+    Board.create(coord:'73', is_occupied: true, game_id: 1)
+    Board.create(coord:'75', is_occupied: true, game_id: 1)
+    Board.create(coord:'77', is_occupied: true, game_id: 1)
+    # Top player (Red)
+    Piece.create(location: '00', is_king: false, game_id: 1, unique_piece_id: 'R1')
+    Piece.create(location: '02', is_king: false, game_id: 1, unique_piece_id: 'R2')
+    Piece.create(location: '04', is_king: false, game_id: 1, unique_piece_id: 'R3')
+    Piece.create(location: '06', is_king: false, game_id: 1, unique_piece_id: 'R4')
+    Piece.create(location: '11', is_king: false, game_id: 1, unique_piece_id: 'R5')
+    Piece.create(location: '13', is_king: false, game_id: 1, unique_piece_id: 'R6')
+    Piece.create(location: '15', is_king: false, game_id: 1, unique_piece_id: 'R7')
+    Piece.create(location: '17', is_king: false, game_id: 1, unique_piece_id: 'R8')
     Piece.create(location: '20', is_king: false, game_id: 1, unique_piece_id: 'R9')
     Piece.create(location: '33', is_king: false, game_id: 1, unique_piece_id: 'R10')
     Piece.create(location: '35', is_king: false, game_id: 1, unique_piece_id: 'R11')
     Piece.create(location: '26', is_king: false, game_id: 1, unique_piece_id: 'R12')
-
+    # Bottom player (Black)
     Piece.create(location: '51', is_king: false, game_id: 1, unique_piece_id: 'B12')
     Piece.create(location: '53', is_king: false, game_id: 1, unique_piece_id: 'B11')
     Piece.create(location: '44', is_king: false, game_id: 1, unique_piece_id: 'B10')
     Piece.create(location: '46', is_king: false, game_id: 1, unique_piece_id: 'B9')
+    Piece.create(location: '60', is_king: false, game_id: 1, unique_piece_id: 'B8')
+    Piece.create(location: '62', is_king: false, game_id: 1, unique_piece_id: 'B7')
+    Piece.create(location: '64', is_king: false, game_id: 1, unique_piece_id: 'B6')
+    Piece.create(location: '66', is_king: false, game_id: 1, unique_piece_id: 'B5')
+    Piece.create(location: '71', is_king: false, game_id: 1, unique_piece_id: 'B4')
+    Piece.create(location: '73', is_king: false, game_id: 1, unique_piece_id: 'B3')
+    Piece.create(location: '75', is_king: false, game_id: 1, unique_piece_id: 'B2')
+    Piece.create(location: '77', is_king: false, game_id: 1, unique_piece_id: 'B1')
 
     it "should return true if the square being jumped is occupied by the opponent's piece" do
-
       expect(Game.opponent_in_jump_midpoint?('35', '57', 'R11')).to eq true
     end
     it "should return true if the square being jumped is occupied by the opponent's piece" do
@@ -175,7 +219,7 @@ describe Game do
       expect(Game.opponent_in_jump_midpoint?('20', '42', 'R9')).to eq false
     end
     it "should return false if the square has the player's own piece" do
-      expect(Game.opponent_in_jump_midpoint?('26', '44', 'B12')).to eq false
+      expect(Game.opponent_in_jump_midpoint?('60', '42', 'B8')).to eq false
     end
   end
 end
