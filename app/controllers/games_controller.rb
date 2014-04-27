51a65c501ee677bc73c6cb9cd7a27f1ded0e94a3
Javad Afshar <mjafshar@gmail.com>
2014-04-27 14:56:49 -0500

    Moved Game.valid_move?(start_loc, end_loc, unique_piece_id) call deeper into the validations.
    New location updates weren't saving to DB, using #update method instead of .attr = arg
    
    Added puts tests into controllers
    
    Added improved .empty_space? by checking for the squares existance.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,33 +8,50 @@ class GamesController < ApplicationController
     unique_piece_id = params[:unique_piece_id]
     midpoint = Game.find_square_between_origin_and_destination(start_loc, end_loc)
 
-    if Game.empty_space?(end_loc) && Game.valid_move?(start_loc, end_loc, unique_piece_id)
+    if Game.empty_space?(end_loc) #&& Game.valid_move?(start_loc, end_loc, unique_piece_id)
       move_distance = Game.move_distance_calculation(start_loc, end_loc, unique_piece_id)
+      if (move_distance.abs == 1) && Game.valid_move?(start_loc, end_loc, unique_piece_id)
+          Board.where(coord: start_loc, game_id: game_id).first.update(is_occupied: false)
+          Board.where(coord: end_loc, game_id: game_id).first.update(is_occupied: true)
+          Piece.where(unique_piece_id: unique_piece_id, game_id: game_id).first.update(location: end_loc)
 
-      if move_distance == 1
-          Board.where(coord: start_loc, game_id: game_id).first.is_occupied = false
-          Board.where(coord: end_loc, game_id: game_id).first.is_occupied = true
-          Piece.where(unique_piece_id: unique_piece_id, game_id: game_id).first.location = end_loc
-
+          # puts '==========================='
+          # puts 'This is true, yes you can make this basic ass move'
+          # puts '==========================='
           return 'true'
-      elsif move_distance == 2
-        if Game.opponent_in_jump_midpoint?(start_loc, end_loc)
-          Board.where(coord: start_loc, game_id: game_id).first.is_occupied = false
-          Board.where(coord: end_loc, game_id: game_id).first.is_occupied = true
-          Piece.where(unique_piece_id: unique_piece_id, game_id: game_id).first.location = end_loc
+      elsif (move_distance.abs == 2) && Game.valid_move?(start_loc, end_loc, unique_piece_id)
+        if Game.opponent_in_jump_midpoint?(start_loc, end_loc, unique_piece_id)
+          Board.where(coord: start_loc, game_id: game_id).first.update(is_occupied: false)
+          Board.where(coord: end_loc, game_id: game_id).first.update(is_occupied: true)
+          Piece.where(unique_piece_id: unique_piece_id, game_id: game_id).first.update(location: end_loc)
 
-          Board.where(coord: midpoint, game_id: game_id).first.is_occupied = false
-          Piece.where(position: midpoint, game_id: game_id).first.location = ''
+          Board.where(coord: midpoint, game_id: game_id).first.update(is_occupied: false)
+          Piece.where(location: midpoint, game_id: game_id).first.update(location: '')
 
+          # puts '==========================='
+          # puts 'This is true, yes you can jump'
+          # puts '==========================='
           return 'true'
         else
-          # @error = "Invalid move!"
+          # puts '==========================='
+          # puts 'This is false, you cannot make this jump'
+          # puts '==========================='
+          # @error = "Can't jump this space!"
           return 'false'
         end
       else
-        # @error = "Invalid move!"
+        # puts '==========================='
+        # puts "This is false, wrong number of moves: #{move_distance}"
+        # puts '==========================='
+        # @error = "Wrong number of moves!"
         return 'false'
       end
+    else
+      # puts '==========================='
+      # puts 'This is false, your destination is occupied'
+      # puts '==========================='
+      # @error = "Destination is occupied!"
+      return false
     end
   end
 end
